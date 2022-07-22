aac3e6056294a358165457de615752c13945ca18
Antonella D'Atri <antonella.datri@alu.unq.edu.ar>
2022-07-22 19:23:18 -0300

    try deploy heroku

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,19 +7,18 @@ function App() {
             <h2 className="mb-5">Conocimientos</h2>
             <div className="subheading mb-3">Lenguajes de programacion</div>
             <ul className="list-inline dev-icons">
-                
-                <li className="list-inline-item"><Icon.Html5/></li>
+                {/* <li className="list-inline-item"><Icon.Html5/></li>
                 <li className="list-inline-item"><Icon.Css3/></li>
                 <li className="list-inline-item"><Icon.Js/></li>
                 <li className="list-inline-item"><Icon.Npm/></li>
                 <li className="list-inline-item"><Icon.React/></li>
                 <li className="list-inline-item"><Icon.Symfony/></li>
-                <li className="list-inline-item"><Icon.Spring/></li>
+                <li className="list-inline-item"><Icon.Spring/></li> */}
                 <li className="list-inline-item"><Icon.Bootstrap/></li>
-                <li className="list-inline-item"><Icon.Java/></li>
+                {/* <li className="list-inline-item"><Icon.Java/></li>
                 <li className="list-inline-item"><Icon.MySQL/></li>
                 <li className="list-inline-item"><Icon.Kotlin/></li>
-                <li className="list-inline-item"><Icon.PostgreSQL/></li>
+                <li className="list-inline-item"><Icon.PostgreSQL/></li> */}
             </ul>
             <div className="subheading mb-3">Otros conocimientos</div>
             <ul className="fa-ul mb-0">
